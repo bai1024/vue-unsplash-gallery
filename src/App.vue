6c2cce6a4c12b8a61a793087c1c3a9eaa0517ab0
Bai Yan <dingdingbai0424@gmail.com>
2017-06-25 11:53:36 +0800

    implement zoom in pic

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template lang='pug'>
-  <div id="app">
-    h1 Unsplash Gallery
-    .gallery__zoomin(v-bind:style="{backgroundImage:'url(' + url + ')'}")
+  #app
+    .gallery__header
+      h1 Unsplash Gallery
+      h2 Beautiful,free photos. 
+        br 
+        | Gifted by the world's most generous community of photographers. 🎁
+    .gallery__zoomin(v-bind:class="{ gallery__zoomin__hide:!isActive }",v-bind:style="{backgroundImage:'url(' + url + ')'}",v-on:click="isActive = false;url = null")
     .gallery__layout
-      <svg v-bind:class="{'gallery__layout__btn--active': isActive }" v-on:click="isActive = true" version="1.1" viewBox="0 0 32 32" width="32" height="32" aria-labelledby="icon-title-4721 icon-desc-4722" aria-hidden="false" data-reactid=".rwftimv01s.0.4.0.3.0.1.$single.0"><path d="M30 14c1.1 0 2-.9 2-2v-10c0-1.1-.9-2-2-2h-28c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2m0 18c-1.1 0-2-.9-2-2v-10c0-1.1.9-2 2-2h28c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2" data-reactid=".rwftimv01s.0.4.0.3.0.1.$single.0.1"></path></svg>
+      <svg v-bind:class="{'gallery__layout__btn--active': isDefault }" v-on:click="isDefault = true" version="1.1" viewBox="0 0 32 32" width="32" height="32" aria-labelledby="icon-title-4721 icon-desc-4722" aria-hidden="false" data-reactid=".rwftimv01s.0.4.0.3.0.1.$single.0"><path d="M30 14c1.1 0 2-.9 2-2v-10c0-1.1-.9-2-2-2h-28c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2m0 18c-1.1 0-2-.9-2-2v-10c0-1.1.9-2 2-2h28c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2" data-reactid=".rwftimv01s.0.4.0.3.0.1.$single.0.1"></path></svg>
       
-      <svg version="1.1"  v-bind:class="{'gallery__layout__btn--active': !isActive }" v-on:click="isActive = false" viewBox="0 0 32 32" width="32" height="32" aria-labelledby="icon-title-4723 icon-desc-4724" aria-hidden="false" data-reactid=".rwftimv01s.0.4.0.3.0.1.$multi.0"><path d="M0 2v10c0 1.106 0.896 2 2 2h10c1.104 0 2-0.894 2-2v-10c0-1.106-0.896-2-2-2h-10c-1.104 0-2 0.894-2 2zM2 18c-1.104 0-2 0.894-2 2v10c0 1.106 0.896 2 2 2h10c1.104 0 2-0.894 2-2v-10c0-1.106-0.896-2-2-2h-10zM20 18c-1.106 0-2 0.894-2 2v10c0 1.106 0.894 2 2 2h10c1.106 0 2-0.894 2-2v-10c0-1.106-0.894-2-2-2h-10zM20 0c-1.106 0-2 0.894-2 2v10c0 1.106 0.894 2 2 2h10c1.106 0 2-0.894 2-2v-10c0-1.106-0.894-2-2-2h-10z" data-reactid=".rwftimv01s.0.4.0.3.0.1.$multi.0.1"></path></svg>
+      <svg version="1.1"  v-bind:class="{'gallery__layout__btn--active': !isDefault }" v-on:click="isDefault = false" viewBox="0 0 32 32" width="32" height="32" aria-labelledby="icon-title-4723 icon-desc-4724" aria-hidden="false" data-reactid=".rwftimv01s.0.4.0.3.0.1.$multi.0"><path d="M0 2v10c0 1.106 0.896 2 2 2h10c1.104 0 2-0.894 2-2v-10c0-1.106-0.896-2-2-2h-10c-1.104 0-2 0.894-2 2zM2 18c-1.104 0-2 0.894-2 2v10c0 1.106 0.896 2 2 2h10c1.104 0 2-0.894 2-2v-10c0-1.106-0.896-2-2-2h-10zM20 18c-1.106 0-2 0.894-2 2v10c0 1.106 0.894 2 2 2h10c1.106 0 2-0.894 2-2v-10c0-1.106-0.894-2-2-2h-10zM20 0c-1.106 0-2 0.894-2 2v10c0 1.106 0.894 2 2 2h10c1.106 0 2-0.894 2-2v-10c0-1.106-0.894-2-2-2h-10z" data-reactid=".rwftimv01s.0.4.0.3.0.1.$multi.0.1"></path></svg>
     
     template(v-if="photos")
-      .gallery__list__container(v-if="isActive")
+      .gallery__list__container(v-if="isDefault")
         photo-list(
           v-for="(photo,index) in photos",
           :key="photo.id",
@@ -17,7 +21,7 @@
           v-on:listenToChildEvent="showMsg"
         )
 
-      .gallery__grid__container(v-if="!isActive")
+      .gallery__grid__container(v-if="!isDefault")
         .gallery__grid
           photo-grid(
             v-for="photo in data1",
@@ -54,12 +58,13 @@ export default {
   data() {
     return {
       page: 1,
-      isActive: true,
+      isDefault: true,
       photos:null,
       data1:null,
       data2:null,
       data3:null,
-      url:null
+      url:null,
+      isActive:false
     }
   },
   methods:{
@@ -82,9 +87,8 @@ export default {
 
     },
     showMsg:function(data){
-      console.log(data)
       this.url = data
-      console.log(this.url)
+      this.isActive = true
     }
   },
   created(){
@@ -111,20 +115,33 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  
+  .gallery__header
+    padding-top: 60px;
+    padding-bottom: 72px;
+    text-align: left;
+    > h2
+      font-size: 18px
+      font-weight: 400
+      color: #111
+
   .gallery__zoomin
     position: fixed;
     top: 0;
     left: 0;
     width: 100vw;
     min-height: 100vh;
-    pointer-events: none
     z-index: 6;
     background-position: 50%;
     background-size: cover
     overflow: auto
+    cursor: zoom-out
+    
+  .gallery__zoomin__hide
+    pointer-events: none
+    
   .gallery__layout
     margin-bottom: 24px;
+    text-align: right
     svg
       width: 20px;
       fill: gray;
